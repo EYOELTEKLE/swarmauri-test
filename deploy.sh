b2b0a7eaa7c69c82d25faca45c7151a03d40adcc
EYOELTEKLE <eyoeltekle10@gmail.com>
2024-10-16 21:43:26 +0300

    fix: dotenv installation issue in debian system

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,12 @@ sudo apt install python3 python3-pip -y
 
 # Set up a virtual environment
 python3 -m venv venv
+apt install python3-dotenv
 source venv/bin/activate
 
 # Install dependencies
 pip install -r requirements.txt
+pip install dotenv
 
 # Run the application (assuming you have a run.py)
 python run.py
